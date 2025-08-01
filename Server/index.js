bf792f2c3a2f8c5722dd0ba070d0647f60bbdd77
Samson <samsonm08@gmail.com>
2025-08-01 14:30:33 -0500

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -17,7 +17,7 @@ server.use(express.json());
 
 server.use(
   cors({
-    origin: "https://fhotoshtock.netlify.app/",
+    origin: "https://fhotoshtock.netlify.app",
     credentials: true,
   })
 

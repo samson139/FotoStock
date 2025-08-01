7e4088e5b96a44063d43e599ff95546af455b90a
samson139 <samsonm08@gmail.com>
2025-08-01 14:53:25 -0500

    added config

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,10 +1,23 @@
+
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+import { resolve } from 'path';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   plugins: [
-    react(),  // React plugin
-  ]
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: '_redirects',
+          dest: '.' // puts it into dist/
+        }
+      ]
+    })
+  ],
+  build: {
+    outDir: 'dist',
+  }
 });
-

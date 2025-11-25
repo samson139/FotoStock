e28429340ae6b54611b5d7b3f722309fc6e5d0f3
samson139 <samsonm08@gmail.com>
2025-11-25 08:44:09 -0600

    fixed cors issue

--- Server/Controllers/Signin.js
+++ Server/Controllers/Signin.js
@@ -1,49 +1,56 @@
-const { checkPreviousUser } = require("../Controllers/Signup")
-const bcrypt = require('bcrypt');
+const { checkPreviousUser } = require("../Controllers/Signup");
+const bcrypt = require("bcrypt");
 const UserModel = require("../models/usermodel");
 const jwt = require("jsonwebtoken");
-require('dotenv').config();
+require("dotenv").config();
 
 const signin = async (req, res) => {
   const { email, password } = req.body;
 
+  // Check if user exists
+  if (!(await checkPreviousUser(email))) {
+    return res.status(400).json({ message: "User doesn't exist" });
+  }
 
-  if (!await checkPreviousUser(email)) return res.status(400).json({ message: 'User Doesnt exist' })
   try {
-    const user = await UserModel.findOne({ email: email })
-    const passwordMatch = await bcrypt.compare(password, user.password);
-    if (passwordMatch) {
-      const signinToken = jwt.sign({
-        id: user._id,
-        firstname: user.firstname,
-        lastname: user.lastname
-      },
-        process.env.SECRETKEY,
-
-        { expiresIn: "1d" });
-      const isProduction = process.env.NODE_ENV === "production";
-
-      res.cookie("jwtToken", signinToken, {
-        httpOnly: true,
-        secure: isProduction,                // true for HTTPS (prod), false for localhost
-        sameSite: isProduction ? "None" : "Lax", // None for cross-origin, Lax for localhost
-        maxAge: 1000 * 60 * 30
-      });
-
-      return res.status(200).json({
-        message: "Logged in successfully"
-      });
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "User doesn't exist" });
     }
-    else {
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
       return res.status(401).json({ message: "Wrong Password" });
     }
-  }
-  catch (error) {
 
-    return res.status(500).json({ message: "An error occured and the code is " });
-  }
-}
+    // Create JWT token
+    const signinToken = jwt.sign(
+      {
+        id: user._id,
+        firstname: user.firstname,
+        lastname: user.lastname,
+      },
+      process.env.SECRETKEY,
+      { expiresIn: "1d" }
+    );
 
-module.exports = {
-  signin
-}
+    // Set secure cookie (mobile + Vercel compatible)
+    res.cookie("jwtToken", signinToken, {
+      httpOnly: true,
+      secure: true,          // MUST be true for mobile + HTTPS
+      sameSite: "None",      // MUST be None for cross-site cookies
+      path: "/",             // Important for cookie visibility everywhere
+      maxAge: 1000 * 60 * 30 // 30 minutes
+    });
+
+    return res.status(200).json({ message: "Logged in successfully" });
+
+  } catch (error) {
+    console.error("Signin error:", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+module.exports = { signin };
+
